d20627d0c2c9e686d3a8a9bb19f2eefd43058e44
Elijah <elijahsharper1@gmail.com>
2018-02-04 11:48:05 +0300

    Coupon test. Validation and AJAX testing

---
+++ src/components/CouponCode.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input type="text" class="coupon-code" v-model="code" @input="validate">
+
+    <p v-if="valid">
+      Coupon Redeemed: {{ message }}
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        valid: false,
+        code: '',
+        coupons: [
+          {
+            code: '50OFF',
+            message: '50% Off!',
+            discount: 50
+          },
+          {
+            code: 'FREE',
+            message: 'Entirely Free!',
+            discount: 100
+          }
+        ]
+      }
+    },
+
+    computed: {
+      message () {
+        return this.coupons.find(coupon => coupon.code === this.code).message
+      }
+    },
+
+    methods: {
+      validate () {
+        this.valid = this.coupons.map(coupon => coupon.code).includes(this.code);
+      }
+    }
+  }
+</script>
