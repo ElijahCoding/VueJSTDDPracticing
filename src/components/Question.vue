96a712868fa66f617b021db8162c79b16823c554
Elijah <elijahsharper1@gmail.com>
2018-02-04 17:11:55 +0300

    Hide-Show test

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -7,18 +7,22 @@
       <button id="edit" @click="editing = true">Edit</button>
     </div>
 
-    <div v-else>
+    <div v-if="editing">
       <input type="text" name="title" v-model="question.title" />
+      <textarea name="body" v-model="question.body"></textarea>
+
+      <button id="update" @click="editing = false"></button>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    props: ['question'],
+    props: ['dataQuestion'],
 
     data () {
       return {
+        question: this.dataQuestion,
         editing: false
       }
     }
--- test/Question.spec.js
+++ test/Question.spec.js
@@ -8,7 +8,7 @@ describe ('Question', () => {
   beforeEach(() => {
     wrapper = mount(Question, {
       propsData: {
-        question: {
+        dataQuestion: {
           title: 'The title',
           body: 'The body'
         }
@@ -29,11 +29,44 @@ describe ('Question', () => {
     wrapper.find('#edit').trigger('click');
 
     expect(wrapper.find('input[name=title]').element.value).toBe('The title');
+    expect(wrapper.find('textarea[name=body]').element.value).toBe('The body');
+  });
+
+  it ('hides the edit button during edit mode', () => {
+    wrapper.find('#edit').trigger('click');
+
+    expect(wrapper.contains('#edit')).toBe(false);
+  });
+
+  it ('updateds the question after being edited', () => {
+    wrapper.find('#edit').trigger("click");
+
+    type('input[name=title]', 'Changed title');
+    type('textarea[name=body]', 'Changed body');
+    // wrapper.find('input[name=title]').element.value = 'Changed title';
+    // wrapper.find('input[name=title]').trigger('input');
+
+    click('#update');
+    // wrapper.find('#update').trigger('click');
+
+    see('Changed title');
+    see('Changed body');
   });
 
   let see = (text, selector) => {
     let wrap = selector ? wrapper.find(selector) : wrapper;
 
     expect(wrapper.html()).toContain(text);
+  }
+
+  let type = (selector, text) => {
+    let node = wrapper.find(selector);
+
+    node.element.value = text;
+    node.trigger('input');
+  }
+
+  let click = (selector) => {
+    wrapper.find(selector).trigger('click');
   }
 });
